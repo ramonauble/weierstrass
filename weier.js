96158051371b7c507b63ba7d44f614fbb6753672
ramona <ramonaauble@gmail.com>
2021-05-13 12:33:22 -0400

    bugfix for symmetry, change drawing width to variable, slider change event handlers

--- weier.js
+++ weier.js
@@ -5,27 +5,29 @@ $(document).ready(function() {
   const wCanvW = weierCanv.width;
   const wCanvH = weierCanv.height;
   weierCanvCtx.fillStyle = "#5D2E7B";
-  weierCanvCtx.lineWidth = 1;
+  weierCanvCtx.lineWidth = .8;
   weierCanvCtx.strokeStyle = "#FFFFFF";
   weierCanvCtx.font = "30px monospace";
   weierCanvCtx.textAlign = "center";
 
   var weierCurve = [];
-  var a = .24;
-  var b = 2400;
+  var a = .69;
+  var b = 7200;
   var locX;
   var max = 0;
-  var xInc = wCanvW/2048.0;
+  var samples = 4096.0;
+  var xInc = wCanvW/samples;
+  var xWidth = 16;
 
   var lastUpdate;
   var updateTime = 33.333333; //ms
   function drawCanvas(timestamp) {
     if (lastUpdate == undefined || (timestamp - lastUpdate) > updateTime) {
       lastUpdate = timestamp; //record latest update time
-      for (let x = 0; x < 2048; x++) {
-        locX = 2*(x/2047) - 1; //normalize x to [-1, 1];
+      for (let x = 0; x <= samples; x++) {
+        locX = xWidth*(x/samples) - (xWidth/2); //normalize x to [-1, 1];
         weierCurve[x] = 0; //clear accumulator
-        for (let n = 0; n < 16; n++) {
+        for (let n = 0; n < 8; n++) {
           weierCurve[x] += Math.pow(a, n) * Math.cos((b**n)*locX*Math.PI);
         }
         if (Math.abs(weierCurve[x]) > max) { //discern max for each frame
@@ -35,8 +37,8 @@ $(document).ready(function() {
       let wX = 0;
       let wY = 0;
       weierCanvCtx.beginPath();
-      for (let c = 0; c < 2047; c++) {
-        wY = (((weierCurve[c]/max) + 1)/2)*wCanvH;
+      for (let c = 0; c <= samples; c++) {
+        wY = wCanvH - (((weierCurve[c]/max) + 1)/2)*wCanvH;
         if (c == 0) {
           weierCanvCtx.moveTo(wX, wY);
         } else {
@@ -51,4 +53,17 @@ $(document).ready(function() {
     window.requestAnimationFrame(drawCanvas);
   }
   window.requestAnimationFrame(drawCanvas);
+
+  $("#aSlider").on("input", function() {
+    let $this = $(this);
+    a = $this.val();
+  });
+  $("#bSlider").on("input", function() {
+    let $this = $(this);
+    b = $this.val();
+  });
+  $("#xSlider").on("input", function() {
+    let $this = $(this);
+    xWidth = $this.val();
+  });
 });
